6f07b2ae5379ccc9eca91dfcd8a93ac693a900cb
divyam <divyam1000mishra@gmail.com>
2024-01-09 23:54:38 +0530

    again3

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-    origin: 'https://main--dreamy-speculoos-c0eaa3.netlify.app/', // or your frontend's origin
+    origin: 'https://dreamy-speculoos-c0eaa3.netlify.app/', // or your frontend's origin
   }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
