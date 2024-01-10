49fe17d28d6a250f4d0899b25f030f49d5322005
divyam <divyam1000mishra@gmail.com>
2024-01-10 22:24:21 +0530

    render1111

--- app/src/scenes/loginPage/Form.jsx
+++ app/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://mern-app-limf.onrender.com//auth/register",
+      "https://mern-app-limf.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("https://mern-app-limf.onrender.com//auth/login", {
+    const loggedInResponse = await fetch("https://mern-app-limf.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
