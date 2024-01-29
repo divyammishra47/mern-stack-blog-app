d111c6ddd150e8f812d101d124e6861acb7b56d6
divyam <divyam1000mishra@gmail.com>
2024-01-29 13:55:23 +0530

    star

--- app/src/components/Friend.jsx
+++ app/src/components/Friend.jsx
@@ -43,7 +43,6 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
         <Box
           onClick={() => {
             navigate(`/profile/${friendId}`);
-            navigate(0);
           }}
         >
           <Typography
